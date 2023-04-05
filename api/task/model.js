01537546443da60f949e655433b8f29d8d832c82
İrem Çelebi <iremcelebi2404@gmail.com>
2023-04-05 03:17:35 +0300

    1 test kaldı

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,49 @@
-// bu`Task` modeli buraya
+const db = require("../../data/dbConfig");
+
+async function getTasks() {
+  const tasks = await db("tasks")
+    .select(
+      "tasks.task_id",
+      "tasks.task_description",
+      "tasks.task_notes",
+      "tasks.task_completed",
+      "projects.project_name",
+      "projects.project_description"
+    )
+    .leftJoin("projects", "projects.project_id", "tasks.project_id");
+  //   console.log(tasks);
+  tasks.forEach((task) => {
+    if (
+      task.task_completed == 1 ||
+      task.task_completed == true ||
+      task.task_completed == "true"
+    ) {
+      task.task_completed = true;
+    } else {
+      task.task_completed = false;
+    }
+  });
+  return tasks;
+}
+
+async function addTask(taskobj) {
+  await db("tasks").insert(taskobj);
+
+  const response = await db("tasks")
+    .where("task_description", taskobj.task_description)
+    .first();
+
+  if (
+    taskobj.task_completed == 1 ||
+    taskobj.task_completed == true ||
+    taskobj.task_completed == "true"
+  ) {
+    response.task_completed = true;
+  } else {
+    response.task_completed = false;
+  }
+
+  return await response;
+}
+
+module.exports = { addTask, getTasks };
--- api/task/router.js
+++ api/task/router.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const taskModel = require("./model");
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
-    res.json({ message: "selamlar task" });
+    const tasks = await taskModel.getTasks();
+    res.json(tasks);
   } catch (err) {
     next(err);
   }
 });
 
+router.post("/", async (req, res, next) => {
+  try {
+    const task = await taskModel.addTask(req.body);
+    res.json(task);
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = router;
